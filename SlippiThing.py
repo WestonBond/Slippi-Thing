923bcf68401296a8e659e40fc60c2d2a15ecf934
WestonBond <owenmctoen@gmail.com>
2022-10-31 21:21:29 -0400

    Update SlippiThing.py

--- SlippiThing.py
+++ SlippiThing.py
@@ -6,6 +6,8 @@ import os
 import json
 import tkinter.messagebox
 
+checker = False
+
 #Create an instance of Tkinter frame
 win= Tk()
 
@@ -18,22 +20,33 @@ if os.stat("SlippiPath.txt") != 0:
     with open("SlippiPath.txt") as slipPath:
         userPath = slipPath.readline()
 
+def setPath():
+    if os.path.exists(entryU.get()):
+        userPath = entryU.get()
+
+        with open("SlippiPath.txt", "a") as slipPath:   
+            slipPath.write(entryU.get())
+        
+        for widget in win.winfo_children():
+            widget.destroy()
+
+        checker = True
+
+    else:
+        print(entryU.get())
+        tkinter.messagebox.showinfo("Incorrect File",  "Your chosen file path does not exist.")
 
 if userPath == "":
-    labelU = Label(win, text="Enter User Path:", font=("Courier 15 bold"))
+    labelU = Label(win, text="Enter user.json Path:", font=("Courier 15 bold"))
     labelU.pack()
 
     entryU = Entry(win, width= 40)
     entryU.focus_set()
     entryU.pack()
 
-    if os.stat("SlippiPath.txt") != 0:
-        userPath = entryU.get()
 
-        with open("SlippiPath.txt", "a") as slipPath:   
-            slipPath.write(entryU.get())
-    else:
-        tkinter.messagebox.showinfo("Incorrect File",  "Your chosen file path does not exist.")
+    e = Button(win, text="Enter", font=("Courier 15 bold"), command=lambda : setPath())
+    e.pack(side = BOTTOM, pady=10)
 
 
 def makeDo(list):
@@ -51,13 +64,6 @@ def makeDo(list):
 
     print(list)
 
-if os.stat("SlippiStuff.txt") != 0:
-    with open("SlippiStuff.txt") as slipFile:
-        for line in slipFile:
-            lSplit = line.split("+")
-            eList = lSplit[1][2:len(lSplit[1]) - 1].split('"')
-            e = Button(win, text=lSplit[0], font=("Courier 15 bold"), command=lambda j=eList: makeDo(j))
-            e.pack(side = BOTTOM, pady=10)
 
 def make_button():
 
@@ -69,24 +75,34 @@ def make_button():
         slipPut.write(entryN.get() + "+" + entryD.get())
 
     
+while checker == True:
+    if os.stat("SlippiStuff.txt") != 0:
+        with open("SlippiStuff.txt") as slipFile:
+            for line in slipFile:
+                lSplit = line.split("+")
+                eList = lSplit[1][2:len(lSplit[1]) - 1].split('"')
+                e = Button(win, text=lSplit[0], font=("Courier 15 bold"), command=lambda j=eList: makeDo(j))
+                e.pack(side = BOTTOM, pady=10)
 
-#Initialize a Label to display the User Input
-labelN = Label(win, text="Name", font=("Courier 15 bold"))
-labelN.pack()
+    #Initialize a Label to display the User Input
+    labelN = Label(win, text="Name", font=("Courier 15 bold"))
+    labelN.pack()
 
-entryN = Entry(win, width= 40)
-entryN.focus_set()
-entryN.pack()
+    entryN = Entry(win, width= 40)
+    entryN.focus_set()
+    entryN.pack()
 
-labelD = Label(win, text="Data", font=("Courier 15 bold"))
-labelD.pack()
+    labelD = Label(win, text="Data", font=("Courier 15 bold"))
+    labelD.pack()
 
-#Create an Entry widget to accept User Input
-entryD = Entry(win, width= 40)
-entryD.focus_set()
-entryD.pack()
+    #Create an Entry widget to accept User Input
+    entryD = Entry(win, width= 40)
+    entryD.focus_set()
+    entryD.pack()
 
-#Create a Button to validate Entry Widget
-ttk.Button(win, text= "Create New Account",width= 20, command=lambda : make_button()).pack(pady=5)
+    #Create a Button to validate Entry Widget
+    ttk.Button(win, text= "Create New Account",width= 20, command=lambda : make_button()).pack(pady=5)
+
+    checker = False
 
 win.mainloop()
